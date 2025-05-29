01a2ad4f3284d6bc918d645ac954e867296262cb
Vámosi László Ádám <vamosilaszloadam@outlook.com>
2025-05-29 19:08:17 +0100

    A hajórakományok adatait táblázatba renderelő függvény elkészült

--- web/src/app.js
+++ web/src/app.js
@@ -8,7 +8,28 @@ function getShipments(){
     .then(result=>{
         console.log(result.data);
         shipmentList=result.data;
+        renderTbody();
     });
 };
+
+function renderTbody(){
+    let rows="";
+    for(let shipment of shipmentList){
+        rows+=`
+            <tr>
+                <td>${shipment.id}</td>
+                <td>${shipment.shipmentId}</td>
+                <td>${shipment.sentDate}</td>
+                <td>${shipment.endDate}</td>
+                <td>${shipment.addressee}</td>
+                <td>${shipment.targetCity}</td>
+                <td>${shipment.createdAt}</td>
+                <td>${shipment.updatedAt}</td>
+            </tr>
+        `;
+        console.log(shipment);
+    }
+    tbody.innerHTML=rows;
+}
 
 getShipments();
